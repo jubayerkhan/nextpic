f59775dac1dcb2d97abb6ed8a5c7e92bd6fcdfce
@jubayer.khan <jubayer.khan@annanovas.com>
2025-08-27 19:07:21 +0600

    hide filter btn update

--- app/components/FilterSecondary.jsx
+++ app/components/FilterSecondary.jsx
@@ -49,7 +49,7 @@ export default function FilterSecondary() {
                 <div className="filter_btn_div mt-4 md:mt-0">
                     <button
                         onClick={() => setShowFilter((prev) => !prev)}
-                        className="filter_btn bg-[#10172A] bg-gradient-to-r hover:from-[#FF4F6D]/10 hover:to-[#6C3DF0]/10 transition-transform"
+                        className={`py-3 md:px-6 px-3 rounded-[10px] flex gap-2.5 items-center text-white font-medium w-full md:w-auto md:mb-0 justify-center hover:bg-gradient-to-r hover:from-[#FF4F6D]/10 hover:to-[#6C3DF0]/10 transition-transform relative cursor-pointer overflow-hidden hover:shadow-lg ${showFilter ? "waved-btn":""}`}
                     >
                         <Image
                             src={filter}
